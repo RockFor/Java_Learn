fab4515a9e4a8b35a8195c293892ca6b1e773b4f
RockFor <igor90bond@gmail.com>
2016-04-06 12:43:37 +0300

    Test commit

--- src/chapter13/WriteDemo.java
+++ src/chapter13/WriteDemo.java
@@ -7,7 +7,7 @@ package chapter13;
 public class WriteDemo {
     public static void main(String[] args) {
         int b;
-        b = 'ý';
+        b = 'À';
         System.out.write(b);
         System.out.write('\n');
     }
